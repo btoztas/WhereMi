628ac54e1c2e86c04a8c3d8daeae3cd6ed302ef0
Bruno Gonçalves <bruno.m.goncalves@ist.utl.pt>
2018-06-13 22:16:19 +0100

    removing files folder

--- restart_db.sh
+++ restart_db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf migrations
 rm -rf storage.db
-rm -rf files
+rm -rf wheremi_app/static/files
 env/bin/python run.py db init
 env/bin/python run.py db migrate
 env/bin/python run.py db upgrade
